89f2b0737f337f4f0e630f99bf46531ab2f5e73a
HaojieZhang123 <haojiezhang123@gmail.com>
2025-04-03 13:26:59 +0200

    [UPD] function to request random emails 10 times

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 const requestTarget = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+const generateMail = (string) => {
+    // ciclo for per ripetere la richiesta 10 volte
+    for(let i=0; i < 10; i++){
+        axios.get(string)
+            .then((resp) => {
+                console.log(resp.data.response)
+                document.getElementById(`email${i}`).innerHTML = resp.data.response;
+            })
+
+    }
+
+}
